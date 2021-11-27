b7719b4119841a40987864cb3840f97ad1167947
Martin <escorpmartin97@gmail.com>
2021-11-27 15:14:34 -0300

    requisito 8

--- model/document/findById.js
+++ model/document/findById.js
@@ -2,6 +2,6 @@ const { ObjectId } = require('mongodb');
 const connection = require('../connection');
 
 const findById = async (collection, id) => (ObjectId.isValid(id)
-  ? (await connection()).collection(collection).findOne({ _id: new ObjectId(id) }) : null);
+  ? (await connection()).collection(collection).findOne({ _id: ObjectId(id) }) : null);
 
 module.exports = findById;
--- service/sales/remove.js
+++ service/sales/remove.js
@@ -1,18 +1,25 @@
+const { ObjectId } = require('mongodb');
+const { INVALID_ID_FORMAT } = require('../../middleware/errorsStatus');
 const Sales = require('../../model/document')('sales');
 const Validation = require('../../middleware/validations/verifications');
 
-const existId = async (salesId) => {
-  console.log('id', salesId);
+const existId = async (id) => {
+  console.log('id', id);
 
-  const findId = await Sales.findById(salesId);
+  const findId = await Sales.findById(id);
   console.log('q', findId);
   const validate = await Validation.deleteId(findId);
   return validate;
 };
 
 const remove = async (itensSold) => {
-  const salesDelete = await Sales.remove(itensSold);
+  const objectId = ObjectId.isValid(itensSold);
+
+  if (!objectId) return INVALID_ID_FORMAT;
+
   const validateId = await existId(itensSold);
+
+  const salesDelete = await Sales.remove(itensSold);
   return validateId.error ? validateId : salesDelete;
 };
 
